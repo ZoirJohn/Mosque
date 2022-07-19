9ddd2e4ed33e70f7f016d131240c818d3153b2cb
ZoirJohn <97283858+ZoirJohn@users.noreply.github.com>
2022-07-19 15:34:35 +0500

    Delete JS directory

--- JS/singlePage.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const postName = document.querySelector("#postName");
-const header = document.querySelector(".breadcrumb__item");
-header.innerHTML = postName.innerHTML.toLowerCase();
